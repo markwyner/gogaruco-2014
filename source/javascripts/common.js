cf926347015543d588d388b60b8dc75dfa9e8d73
Mark Wyner <markwynerdesign@me.com>
2014-03-13 01:20:00 -0700

    Added descriptions/functionality to schedule

--- source/javascripts/common.js
+++ source/javascripts/common.js
@@ -1,5 +1,25 @@
 //= require_tree .
 
+// mobile nav toggle
+
 $('.icon-reorder').click(function() {
 	$('nav ul').fadeToggle();
+});
+
+// schedule session descriptions
+
+$('.sessions h3').click(function() {
+	$(this).siblings('.session-description').fadeToggle();
+});
+
+$('.session-description').prepend('<strong><i class="icon-remove"></i></strong>');
+
+$('.session-description strong').bind('click',function() {
+	$(this).closest('.session-description').fadeToggle();
+});
+
+$(document).keyup(function(e) {
+	if (e.keyCode == 27) {
+		$('.session-description').fadeOut();
+	}
 });
