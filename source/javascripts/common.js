476ed4c876b76e2808ad3f363d2f306ee20f8f81
Mark Wyner <markwynerdesign@me.com>
2014-03-21 15:42:49 -0700

    replaced Google Maps with Mapbox

--- source/javascripts/common.js
+++ source/javascripts/common.js
@@ -45,6 +45,37 @@ $(document).ready(function(){
 		});
 	}
 
+	// map
+
+	var map = L.mapbox.map('map', 'examples.map-9ijuk24y').setView([37.768284, -122.393491], 15);
+	map.scrollWheelZoom.disable();
+	map.dragging.disable();
+	map.touchZoom.disable();
+	var marker = L.mapbox.featureLayer({
+		type: 'Feature',
+		geometry: {
+			type: 'Point',
+			coordinates: [-122.393491, 37.768284]
+		},
+		properties: {
+			title: 'Mission Bay Conference Center at UCSF',
+			description: '1675 Owens Street, Suite 251, San Francisco, CA 94143-3008 | 866-431-8273 | <a href="http://www.acc-missionbayconferencecenter.com/">Website</a>',
+			'marker-size': 'medium',
+			'marker-color': '#e95868',
+			//icon: {
+			//	iconUrl: "/images/robot.svg",
+			//	iconSize: [50, 50], // size of the icon
+			//	iconAnchor: [25, 25], // point of the icon which will correspond to marker's location
+			//	popupAnchor: [0, -25] // point from which the popup should open relative to the iconAnchor
+			//},
+			url: 'http://www.acc-missionbayconferencecenter.com/'
+		}
+	}).addTo(map);
+	marker.eachLayer(function(m) {
+	  m.openPopup();
+	});
+
+
 	// styling iframe content
 	
 	// $('#eventbrite').contents().find('.button_css a').css('background','#000');
